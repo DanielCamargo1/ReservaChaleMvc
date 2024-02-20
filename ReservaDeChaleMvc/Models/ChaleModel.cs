a6a50e27be297cd2404d351ff0df49319f8de7a8
Daniel Camargo <danidcp098@gmail.com>
2024-02-20 16:50:33 -0300

    Start Project

---
+++ ReservaDeChaleMvc/Models/ChaleModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReservaDeChaleMvc.Models
+{
+    public class ChaleModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Precisa que um nome Seja digitado")]
+        public string NomeUsuario { get; set; }
+        public int? QuantidadeDePessoa { get; set; }
+        [Required(ErrorMessage = "Precisa que um chale Seja informado")]
+        public NomeDosChales Chale { get; set; }
+    }
+    public enum NomeDosChales
+    {
+        Fé = 1,
+        RosaDeSaron = 2,
+        EstrelaDaManhã = 3,
+        LirioDosVales = 4,
+        ValeDeJaboque = 5,
+        Luz = 6,
+        Esperança = 7,
+        Amor = 8,
+        Betseda = 9,
+        ColunaDeFogo = 10,
+        LuzDoMundo = 11,
+        Graça = 12,
+        Companheirismo = 13,
+        Paz = 14,
+        Jordão = 15,
+        Jerusalém = 16,
+        Cantico = 17,
+        Belém = 18,
+        MonteSinai = 19,
+        MonteCarmelo = 20
+    }
+
+}
