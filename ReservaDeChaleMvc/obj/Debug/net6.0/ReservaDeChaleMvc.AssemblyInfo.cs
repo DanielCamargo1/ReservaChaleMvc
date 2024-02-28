dee76e6cae8a5700123690f36ae3d238e3ff1fd7
Daniel Camargo <danidcp098@gmail.com>
2024-02-28 13:22:41 -0300

    Resulução de bugs sobre aspController

--- ReservaDeChaleMvc/obj/Debug/net6.0/ReservaDeChaleMvc.AssemblyInfo.cs
+++ ReservaDeChaleMvc/obj/Debug/net6.0/ReservaDeChaleMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReservaDeChaleMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3da3e10a9dcb9dde45c1ff07931eecf833c8d9a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfc20018aefabaf9a4d3ba22b32ad23b5b0bdd74")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReservaDeChaleMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReservaDeChaleMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
